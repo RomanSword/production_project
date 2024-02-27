727cfc2dfdd2162081926bc38f614e2bc72ae559
RomanSword <romakeks1448@gmail.com>
2024-02-27 21:44:20 +0300

    add jest, add classNames test

---
+++ config/jest/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+    preset: 'ts-jest',
+    verbose: true,
+    clearMocks: true,
+    testEnvironment: 'jsdom',
+    coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+    moduleDirectories: ['node_modules'],
+    moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'node'],
+    rootDir: '../../',
+    testMatch: ['<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)']
+};
+
+export default config;
---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,11 @@
+import { classNames } from './classNames';
+
+describe('', () => {
+    test('with two classes', () => {
+        expect(classNames(['class1', 'class2'])).toBe('class1 class2');
+    });
+
+    test('with three classes and false values', () => {
+        expect(classNames(['class1', null, 'class2', '', 'class3'])).toBe('class1 class2 class3');
+    });
+});
