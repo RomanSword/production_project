9dc87b53c1c25de62b12ecd9dc42512e9052f864
RomanSword <romakeks1448@gmail.com>
2024-04-27 07:57:08 +0300

    update ci_actions

--- src/pages/Main/ui/MainPage.async.tsx
+++ src/pages/Main/ui/MainPage.async.tsx
@@ -4,6 +4,7 @@ export const MainPageAsync = lazy(() => {
     return import('./mainPage').then(module => {
         return new Promise(resolve => {
             setTimeout(() => {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 return resolve(module as any);
             }, 1000);
         });
--- src/pages/help/ui/helpPage.async.tsx
+++ src/pages/help/ui/helpPage.async.tsx
@@ -4,6 +4,7 @@ export const HelpPageAsync = lazy(() => {
     return import('./helpPage').then(module => {
         return new Promise(resolve => {
             setTimeout(() => {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
                 return resolve(module as any);
             }, 1000);
         });
