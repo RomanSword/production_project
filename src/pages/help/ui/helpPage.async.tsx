ca2d90dca7a6ad553d23c0098e54870cfb67a7ca
RomanSword <romakeks1448@gmail.com>
2024-05-03 08:37:27 +0300

    try resolve not found module problem

--- src/pages/help/ui/helpPage.async.tsx
+++ src/pages/help/ui/helpPage.async.tsx
@@ -1,15 +1,5 @@
 import { lazy } from 'react';
 
-export const HelpPageAsync = lazy(() => {
-    return import('./helpPage').then(module => {
-        return new Promise(resolve => {
-            setTimeout(() => {
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                return resolve(module as any);
-            }, 1000);
-        });
-    });
+export const HelpPageAsync = lazy(async () => {
+    return await import('./helpPage');
 });
-// export const HelpPageAsync = lazy(async () => {
-//     return await import('./helpPage');
-// });
